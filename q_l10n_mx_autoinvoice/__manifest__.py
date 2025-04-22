2fad99b746614ce2ebe17cacf08259bc1b69013c
Niffy-the-conqueror <117080364+Niffy-the-conqueror@users.noreply.github.com>
2025-04-22 09:09:38 +0200

    corrected manifest

--- q_l10n_mx_autoinvoice/__manifest__.py
+++ q_l10n_mx_autoinvoice/__manifest__.py
@@ -2,7 +2,7 @@
     'name': 'Auto Invoice for CFDI 4.0',
     'category': 'Website',
     'summary': 'Auto Invoice for CFDI 4.0',
-    'version': '15.0.0.0.6',
+    'version': '18.0',
     'development_status': 'Stable',
     'license': 'OPL-1',
     'author': 'QUADIT',
